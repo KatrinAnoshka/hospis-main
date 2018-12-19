18d53fd1c62612a3f7083a112f64fb15192c0bae
KatrinAnoshka <nosha309@mail.ru>
2018-12-19 10:48:17 +0300

    cleaned our news page

--- js/common.js
+++ js/common.js
@@ -1,61 +1,11 @@
 $(document).ready(function() {
 
-  // Slider-main-section
-
-  $('.center').slick({
-      centerMode: true,
-      dots: true,
-      autoplay: true,
-      slidesToShow: 1,
-  });
-  
-  // Sponsors-section
-
-  $('.multiple-items').slick({
-        infinite: true,
-        dots: true,
-        autoplay: true,
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        responsive: [
-    {
-      breakpoint: 1400,
-      settings: {
-        slidesToShow: 5,
-        slidesToScroll: 1,    
-      }
-    },
-    {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1,    
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,      
-      }
-    },
-    {
-      breakpoint: 550,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,     
-      }
-    }
-]
-  });
-
 /*************** Adaptive menu ***************/
 
     (function($){
         $(function() {
             $('.menu__icon').on('click', function() {
                 $(this).closest('.header__bottom').toggleClass('menu_state_open');
-                // $(".button__help").toggleClass('help__open');
             });
         });
     })(jQuery);
@@ -66,5 +16,57 @@ $(document).ready(function() {
         $('.submenu').toggleClass('submenu--active');
         $('.menu__drop-down').toggleClass('menu__drop-down--active');
     });
+    
+/************* Home-page/////Slider-main-section *************/
+
+    $('.center').slick({
+        centerMode: true,
+        dots: true,
+        autoplay: true,
+        slidesToShow: 1,
+    });
+  
+/************** Home-page/////Sponsors-section **************/
+
+    $('.multiple-items').slick({
+        infinite: true,
+        dots: true,
+        autoplay: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        responsive: [
+    {
+        breakpoint: 1400,
+        settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,    
+        }
+    },
+    {
+        breakpoint: 992,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,    
+        }
+    },
+    {
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,      
+        }
+    },
+    {
+        breakpoint: 550,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,     
+      }
+    }
+]
+  });
+
+
+
 
 });
